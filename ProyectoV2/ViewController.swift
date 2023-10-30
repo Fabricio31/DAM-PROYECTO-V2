a6a850895b4811f80dd6bd9b00a9528d3fdb86a5
Fabricio31 <fabriciobecerra18@gmail.com>
2023-10-30 13:47:33 -0500

    Initial Commit

---
+++ ProyectoV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoV2
+//
+//  Created by BigSur on 30/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
